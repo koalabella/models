5a82d3705db4202fe7c10db61ef7c317cbad8775
Ubuntu <ubuntu@ip-172-31-22-123.ec2.internal>
2018-01-09 15:31:58 +0000

    first output

---
+++ research/object_detection/object_detection_bosch.py
@@ -0,0 +1,95 @@
+import numpy as np
+import os
+import sys
+import tensorflow as tf
+
+from collections import defaultdict
+from io import StringIO
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+from PIL import Image
+from glob import glob
+
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
+CKPT = '/home/ubuntu/hdd/test_data/models/r50_bosch_500.pb/frozen_inference_graph.pb'
+PATH_TO_LABELS = '/home/ubuntu/models/research/object_detection/data/traffic_light_label_map.pbtxt'
+NUM_CLASSES = 14
+
+#loading label map
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+print(category_index)
+
+#load forzen model into memory
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+
+    with tf.gfile.GFile(CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+def load_image_into_numpy_array(image):
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape(
+        (im_height, im_width, 3)).astype(np.uint8)
+
+PATH_TO_TEST_IMAGES_DIR = '/home/ubuntu/hdd/test_data/rgb/small_test/'
+
+TEST_IMAGE_PATHS = glob(os.path.join(PATH_TO_TEST_IMAGES_DIR, '*.png'))  # PNG OR JPG
+#TEST_IMAGE_PATHS = glob(os.path.join(PATH_TO_TEST_IMAGES_DIR, '*.jpg'))
+print("Length of test images:", len(TEST_IMAGE_PATHS))
+
+# Size, in inches, of the output images.
+IMAGE_SIZE = (12, 8)
+
+import time
+
+with detection_graph.as_default():
+    with tf.Session(graph=detection_graph) as sess:
+        # Definite input and output Tensors for detection_graph
+        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+        
+        # Each box represents a part of the image where a particular object was detected.
+        detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+        
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+        detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+        for image_path in TEST_IMAGE_PATHS:
+            image = Image.open(image_path)
+            # the array based representation of the image will be used later in order to prepare the
+            # result image with boxes and labels on it.
+            image_np = load_image_into_numpy_array(image)
+            # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+            image_np_expanded = np.expand_dims(image_np, axis=0)
+
+            time0 = time.time()
+
+            # Actual detection.
+            (boxes, scores, classes, num) = sess.run(
+              [detection_boxes, detection_scores, detection_classes, num_detections],
+              feed_dict={image_tensor: image_np_expanded})
+
+            time1 = time.time()
+
+            print("Time in milliseconds", (time1 - time0) * 1000) 
+
+            # Visualization of the results of a detection.
+            vis_util.visualize_boxes_and_labels_on_image_array(
+              image_np,
+              np.squeeze(boxes),
+              np.squeeze(classes).astype(np.int32),
+              np.squeeze(scores),
+              category_index,
+              use_normalized_coordinates=True,
+              line_thickness=8)
+            #plt.legend()
+            plt.savefig(image_path)
